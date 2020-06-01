0367e3d856e2e6e02e718d9a545e1ec4080850d5
Abdullah Manzoor <abdullahpsoft@gmail.com>
2020-06-01 16:46:54 +0500

    pull customer

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -171,7 +171,7 @@
                         </a>
                     </li>
                     <li class="nav-item has-treeview menu-open">
-                        <a href="/agent/{{\Illuminate\Support\Facades\Auth::user()->id}}" class="nav-link">
+                        <a href="/agent" class="nav-link">
                             <i class="nav-icon fas fa-industry"></i>
                             <p>
                                 Agents
@@ -284,6 +284,99 @@
                             <i class="nav-icon fas fa-user"></i>
                             <p>
                                 Reports
+                            </p>
+                        </a>
+                    </li>
+
+                </ul>
+                <ul class="nav nav-treeview">
+                    {{--                        <li class="nav-item">--}}
+                    {{--                            <a href="#" class="nav-link active">--}}
+                    {{--                                <i class="far fa-circle nav-icon"></i>--}}
+                    {{--                                <p>Active Page</p>--}}
+                    {{--                            </a>--}}
+                    {{--                        </li>--}}
+                    {{--                        <li class="nav-item">--}}
+                    {{--                            <a href="#" class="nav-link">--}}
+                    {{--                                <i class="far fa-circle nav-icon"></i>--}}
+                    {{--                                <p>Inactive Page</p>--}}
+                    {{--                            </a>--}}
+                    {{--                        </li>--}}
+                </ul>
+                {{--            </li>--}}
+                {{--                <li class="nav-item">--}}
+                {{--                    <a href="#" class="nav-link">--}}
+                {{--                        <i class="nav-icon fas fa-th"></i>--}}
+                {{--                        <p>--}}
+                {{--                            Simple Link--}}
+                {{--                            <span class="right badge badge-danger">New</span>--}}
+                {{--                        </p>--}}
+                {{--                    </a>--}}
+                {{--                </li>--}}
+                {{--        </ul>--}}
+            </nav>
+            <!-- /.sidebar-menu -->
+        </div>
+
+@endif
+
+@if(\Illuminate\Support\Facades\Auth::user()->role_id == 5)
+    <!-- Sidebar -->
+        <div class="sidebar">
+            <!-- Sidebar user panel (optional) -->
+            <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+                <div class="pull-left image">
+                    <img src="admin-lte/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                </div>
+                <div class="pull-left info">
+                    <p style="color:white">Customer</p>
+                    <div class="" aria-labelledby="">
+                        <a class="" href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </div> {{-- <a href="#"><i class="fa fa-circle text-success"></i> Online</a>  --}}
+
+                </div>
+
+            </div>
+
+
+
+
+            <!-- Sidebar Menu -->
+            <nav class="mt-2">
+                <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                    <!-- Add icons to the links using the .nav-icon class
+                         with font-awesome or any other icon font library -->
+                    <li class="nav-item has-treeview menu-open">
+                        <a href="/dashboard" class="nav-link">
+                            <i class="nav-icon fas fa-tachometer-alt"></i>
+                            <p>
+                                Dashboard
+                            </p>
+                        </a>
+                    </li>
+
+                    <li class="nav-item has-treeview menu-open">
+                        <a href="/customers" class="nav-link">
+                            <i class="nav-icon fas fa-calculator"></i>
+                            <p>
+                                Profile
+                            </p>
+                        </a>
+                    </li>
+
+                    <li class="nav-item has-treeview menu-open">
+                        <a href="/" class="nav-link">
+                            <i class="nav-icon fas fa-user"></i>
+                            <p>
+                                Status
                             </p>
                         </a>
                     </li>
