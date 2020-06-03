eed15295cb5666dabe35130f7aeb82a3f8a9ad7f
Abdullah Manzoor <abdullahpsoft@gmail.com>
2020-06-03 14:36:09 +0500

    super agent push

---
+++ resources/views/customerspool/index.blade.php
@@ -0,0 +1,78 @@
+@extends('layouts.master')
+
+@section('content')
+<div class="box">
+    <div class="box-header">
+      <br>
+      <div class="col-md-12">
+          <div style="float: left">
+            <h3 class="box-title">Customers</h3>
+          </div>
+{{--          <div style="float: right;">--}}
+{{--            <a class="btn btn-success" href="/customers/create"><i class="ion-ios-plus"></i> Add Customers</a>--}}
+{{--          </div>--}}
+      </div>
+        <br>
+    </div>
+    <!-- /.box-header -->
+    <div class="box-body">
+      <table id="example1" class="table table-bordered table-striped">
+        <thead>
+        <tr>
+          <th>Title</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Contact Number</th>
+          <th>Current Status</th>
+          <th>Action</th>
+        </tr>
+        </thead>
+        <tbody>
+          @foreach($customers as $customer)
+          <tr>
+            <td>{{ $customer->title }}</td>
+            <td>{{ $customer->name }}</td>
+            <td>{{ $customer->email }}</td>
+            <td>{{ $customer->contact_number }}</td>
+            @if($customer->current_status == "pending")
+                <td><span class="label label-warning">{{$customer->current_status}}</span></td>
+            @elseif($customer->current_status == "processing")
+                <td><span class="label label-primary">{{$customer->current_status}}</span></td>
+            @elseif($customer->current_status == "confirm")
+                <td><span class="label label-success">{{$customer->current_status}}</span></td>
+            @endif
+            <td>
+
+                <a style="margin-right:20px" href="/customerspool/edit/{{$customer->id}}"><i class="nav-icon fas fa-plus"></i> Add Info</a>
+
+                {{--            <a style="margin-right:20px" href="/customers/edit/{{$customer->id}}"><i class="nav-icon fas fa-edit"></i></a>--}}
+{{--          --}}
+{{--            --}}{{-- <a style="margin-right:20px" href=""> --}}
+{{--              --}}{{-- <i class="nav-icon fas fa-trash"> --}}
+{{--                  <a onclick="clicked({{$customer->id}})" href="#" style="text-decoration: none">--}}
+{{--                    <i class="nav-icon fas fa-trash">--}}
+{{--                      <form method="POST" id="del{{$customer->id}}" action="/customer/{{$customer->id}}">--}}
+{{--                        @csrf--}}
+{{--                        <input type="hidden" name="_method" value="DELETE">--}}
+{{--                      </form>--}}
+{{--                    </i>--}}
+{{--                  </a>--}}
+            </td>
+          </tr>
+          @endforeach
+
+        </tbody>
+      </table>
+    </div>
+    <!-- /.box-body -->
+  </div>
+  <script>
+    function clicked(id){
+        if(confirm("Are You Sure ?")){
+            document.getElementById('del'+id).submit();
+        }
+        else{
+        }
+    }
+  </script>
+@endsection
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -364,7 +364,7 @@
                     </li>
 
                     <li class="nav-item has-treeview menu-open">
-                        <a href="/customers" class="nav-link">
+                        <a href="/customers/edit/{{\Illuminate\Support\Facades\Auth::user()->customer_id}}" class="nav-link">
                             <i class="nav-icon fas fa-calculator"></i>
                             <p>
                                 Profile
@@ -412,5 +412,107 @@
         </div>
 
 @endif
+@if(\Illuminate\Support\Facades\Auth::user()->role_id == 3)
+    <!-- Sidebar -->
+        <div class="sidebar">
+            <!-- Sidebar user panel (optional) -->
+            <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+                <div class="pull-left image">
+                    <img src="admin-lte/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                </div>
+                <div class="pull-left info">
+                    <p style="color:white">Advisor</p>
+                    <div class="" aria-labelledby="">
+                        <a class="" href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </div> {{-- <a href="#"><i class="fa fa-circle text-success"></i> Online</a>  --}}
+
+                </div>
+
+            </div>
+
+
+
+
+            <!-- Sidebar Menu -->
+            <nav class="mt-2">
+                <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                    <!-- Add icons to the links using the .nav-icon class
+                         with font-awesome or any other icon font library -->
+                    <li class="nav-item has-treeview menu-open">
+                        <a href="/dashboard" class="nav-link">
+                            <i class="nav-icon fas fa-tachometer-alt"></i>
+                            <p>
+                                Dashboard
+                            </p>
+                        </a>
+                    </li>
+
+                    <li class="nav-item has-treeview menu-open">
+                        <a href="/customerspool" class="nav-link">
+                            <i class="nav-icon fas fa-calculator"></i>
+                            <p>
+                               Customer Pool
+                            </p>
+                        </a>
+                    </li>
+
+                    <li class="nav-item has-treeview menu-open">
+                        <a href="/customersprocess" class="nav-link">
+                            <i class="nav-icon fas fa-user"></i>
+                            <p>
+                                Processed Customers
+                            </p>
+                        </a>
+                    </li>
+
+                    <li class="nav-item has-treeview menu-open">
+                        <a href="/" class="nav-link">
+                            <i class="nav-icon fas fa-user"></i>
+                            <p>
+                                Track Customer
+                            </p>
+                        </a>
+                    </li>
+
+                </ul>
+                <ul class="nav nav-treeview">
+                    {{--                        <li class="nav-item">--}}
+                    {{--                            <a href="#" class="nav-link active">--}}
+                    {{--                                <i class="far fa-circle nav-icon"></i>--}}
+                    {{--                                <p>Active Page</p>--}}
+                    {{--                            </a>--}}
+                    {{--                        </li>--}}
+                    {{--                        <li class="nav-item">--}}
+                    {{--                            <a href="#" class="nav-link">--}}
+                    {{--                                <i class="far fa-circle nav-icon"></i>--}}
+                    {{--                                <p>Inactive Page</p>--}}
+                    {{--                            </a>--}}
+                    {{--                        </li>--}}
+                </ul>
+                {{--            </li>--}}
+                {{--                <li class="nav-item">--}}
+                {{--                    <a href="#" class="nav-link">--}}
+                {{--                        <i class="nav-icon fas fa-th"></i>--}}
+                {{--                        <p>--}}
+                {{--                            Simple Link--}}
+                {{--                            <span class="right badge badge-danger">New</span>--}}
+                {{--                        </p>--}}
+                {{--                    </a>--}}
+                {{--                </li>--}}
+                {{--        </ul>--}}
+            </nav>
+            <!-- /.sidebar-menu -->
+        </div>
+
+@endif
+
 <!-- /.sidebar -->
 </aside>
